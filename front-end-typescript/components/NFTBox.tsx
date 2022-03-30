6e29900e0fa1ebf82f839b196511a3658de67e63
MATTOP\Matthew <m.durkin92@gmail.com>
2022-03-30 23:02:08 +0100

    Remove comments

--- front-end-typescript/components/NFTBox.tsx
+++ front-end-typescript/components/NFTBox.tsx
@@ -23,39 +23,6 @@ interface NFTBoxProps {
     seller?: string
 }
 
-// type chainType =
-//     | "eth"
-//     | "0x1"
-//     | "ropsten"
-//     | "0x3"
-//     | "rinkeby"
-//     | "0x4"
-//     | "goerli"
-//     | "0x5"
-//     | "kovan"
-//     | "0x2a"
-//     | "polygon"
-//     | "0x89"
-//     | "mumbai"
-//     | "0x13881"
-//     | "bsc"
-//     | "0x38"
-//     | "bsc testnet"
-//     | "0x61"
-//     | "avalanche"
-//     | "0xa86a"
-//     | "avalanche testnet"
-//     | "0xa869"
-//     | "fantom"
-//     | "0xfa"
-//     | undefined
-
-// type tokenIdMetadataParams = {
-//     chain: chainType
-//     address: string
-//     token_id: string
-// }
-
 const truncateStr = (fullStr: string, strLen: number) => {
     if (fullStr.length <= strLen) return fullStr
 

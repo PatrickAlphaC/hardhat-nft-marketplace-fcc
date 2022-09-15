45ac25668f894be05b2f601017458f934bc5e533
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 12:41:21 -0400

    Update cancel-item.js

--- scripts/cancel-item.js
+++ scripts/cancel-item.js
@@ -9,7 +9,7 @@ async function cancel() {
     const tx = await nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)
     await tx.wait(1)
     console.log("NFT Canceled!")
-    if ((network.config.chainId = "31337")) {
+    if ((network.config.chainId == "31337")) {
         await moveBlocks(2, (sleepAmount = 1000))
     }
 }
